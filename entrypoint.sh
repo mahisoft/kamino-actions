8a66eab9bb7d5b493fa85cf710bd0cc5f5f1bf07
Jose Luis Jimenez <jjimenez@mahisoft.com>
2020-08-08 19:09:42 -0400

    cable nexus

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
-echo "${NEXUS_PROPERTIES}" > /build_env/gradle_config/gradle.properties
+PROPERTIES="mahiNexusUsername=${NEXUS_USER}
+mahiNexusPassword=${NEXUS_PASSWORD}
+sharedMavenUrl=https://nexus.mahisoft.com/repository/maven-public/
+snapshotUploadUrl=https://nexus.mahisoft.com/repository/maven-snapshots/
+releaseUploadUrl=https://nexus.mahisoft.com/repository/maven-releases/"
+
+echo "${PROPERTIES}" > /build_env/gradle_config/gradle.properties
 
 SPRING_PROFILES_ACTIVE=build ${GITHUB_WORKSPACE}/gradlew clean check --info
