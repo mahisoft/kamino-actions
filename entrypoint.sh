0d748863ae9873c1833c383bb7370e56cd23a2f8
jjimenez990 <39965433+jjimenez990@users.noreply.github.com>
2020-08-10 12:18:40 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,4 +23,6 @@ docker-compose -f ${GITHUB_WORKSPACE}/docker-compose.yml -f /build_env/env.netwo
 
 docker network connect ${GITHUB_RUN_ID} ${HOSTNAME}
 
-SPRING_PROFILES_ACTIVE=build ${GITHUB_WORKSPACE}/gradlew clean check --info
+# SPRING_PROFILES_ACTIVE=build ${GITHUB_WORKSPACE}/gradlew clean check --info
+
+docker build .
