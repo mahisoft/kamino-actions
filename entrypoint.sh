015b1ca94692942b112806ac754be7d9024b77b7
Jose Luis Jimenez <jjimenez@mahisoft.com>
2020-08-23 20:50:17 -0400

    values

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,9 @@ helm plugin install https://github.com/nouney/helm-gcs
 helm repo add social-coach-stable gs://social-coach/stable
 
 helm repo update
+
+/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file /json_key.json --project kamino-182816
+
+/google-cloud-sdk/bin/gcloud container clusters get-credentials mahisoft-development --zone us-central1-a --project kamino-182816
+
+helm upgrade -i ms-account-service ./social-coach-ms-service-sql-ng-1.0.0.tgz --namespace github -f ms-account-service/values.yml -f ms-account-service/dev.yml --reset-values --set image.tag=latest
